67e94ce74819ea86f07dfbce017802b5267a6040
NADA <haddaji.nada@gmail.com>
2013-01-01 01:11:39 +0100

    #3 creation de la classe domaine, controller et view de Post

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,78 @@
+import ivvq_saveursdicietdailleurs.Commentaire
+import ivvq_saveursdicietdailleurs.Membre
+import ivvq_saveursdicietdailleurs.Post
+
+/*******************************************************************************
+ * SaveurDiciEtDailleurs est une application grails qui consiste à gérer des défis culinaires.
+ * 
+ * Copyright (C) 2012  by - KFITA Nada, CEJALVO Anna, TOURE Mariame, KOFFI Aho Melaine
+ * 
+ * SaveurDiciEtDailleurs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * SaveurDiciEtDailleurs  is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ ******************************************************************************/
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def init = { servletContext ->
+
+		Membre membre1 = new Membre(idMembre : "1", pseudo : "toto", mdp : "123456", prenom : "nado", nom : "nom1", adresse_mail : "nsdkjb@ds.fr")
+		Membre membre2 = new Membre(idMembre : "2", pseudo : "tata", mdp : "123456", prenom : "nada", nom : "nom2", adresse_mail : "nsdkjb@ds.fr")
+		Membre membre3 = new Membre(idMembre : "3", pseudo : "titi", mdp : "123456", prenom : "nadi", nom : "nom3", adresse_mail : "nsdkjb@ds.fr")
+		if (!membre1.save()){
+			membre1.errors.allErrors.each{error -> println error}
+		}
+		if (!membre2.save()){
+			membre2.errors.allErrors.each{error -> println error}
+		}
+		if (!membre3.save()){
+			membre3.errors.allErrors.each{error -> println error}
+		}
+
+		Post post1 = new Post(idPost : 1, intitule : "aaaaaaaaaaaaa", message : "jhkjsjgyugejrg", auteurPost : membre1)
+		Post post2 = new Post(idPost : 2, intitule : "bbbbbbbbbbbbb", message : "jhkjsjgyugejrg", auteurPost : membre1)
+		Post post3 = new Post(idPost : 3, intitule : "ccccccccccccc", message : "jhkjsjgyugejrg", auteurPost : membre2)
+		if (!post1.save()){
+			post1.errors.allErrors.each{error -> println error}
+		}
+		if (!post2.save()){
+			post2.errors.allErrors.each{error -> println error}
+		}
+		if (!post3.save()){
+			post3.errors.allErrors.each{error -> println error}
+		}
+
+
+		Commentaire com1 = new Commentaire(idCommentaire : 1, corpsCommentaire : "un commentaire 1", postCommentaire : post1)
+		Commentaire com2 = new Commentaire(idCommentaire : 2, corpsCommentaire : "un commentaire 2", postCommentaire : post1)
+		Commentaire com3 = new Commentaire(idCommentaire : 3, corpsCommentaire : "un commentaire 3", postCommentaire : post1)
+		if (!com1.save()){
+			com1.errors.allErrors.each{error -> println error}
+		}
+		if (!com2.save()){
+			com2.errors.allErrors.each{error -> println error}
+		}
+		if (!com3.save()){
+			com3.errors.allErrors.each{error -> println error}
+		}
+
+		post1
+				.addToCommentaires(com1)
+				.addToCommentaires(com2)
+				.save()
+		post2
+				.addToCommentaires(com2)
+				.addToCommentaires(com3)
+				.save()
+		post3
+				.addToCommentaires(com3)
+				.addToCommentaires(com1)
+				.save()
+	}
+
+
+	def destroy = {
+	}
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * SaveurDiciEtDailleurs est une application grails qui consiste à gérer des défis culinaires.
+ * 
+ * Copyright (C) 2012  by - KFITA Nada, CEJALVO Anna, TOURE Mariame, KOFFI Aho Melaine
+ * 
+ * SaveurDiciEtDailleurs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * SaveurDiciEtDailleurs  is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+ ******************************************************************************/
 grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
