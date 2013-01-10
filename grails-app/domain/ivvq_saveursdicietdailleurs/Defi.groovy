2e6ae3a0dbb050625e019193da964d36ff5c47c0
Anna <anna.cejalvo@gmail.com>
2013-01-10 10:51:12 +0100

    #7 création de la classe Defi

---
+++ grails-app/domain/ivvq_saveursdicietdailleurs/Defi.groovy
@@ -0,0 +1,18 @@
+package ivvq_saveursdicietdailleurs
+
+class Defi {
+
+	String intitule
+	String description
+	Date dateCreation
+	Date dateLimite
+	//Categorie categorie
+	
+	// relation one-to-many
+	static hasMany = [posts:Post]
+
+	static constraints = {
+		intitule (blank:false)
+		description (widget:'textarea', nullable:true, blank:false)
+	}
+}
---
+++ test/unit/ivvq_saveursdicietdailleurs/DefiTests.groovy
@@ -0,0 +1,17 @@
+package ivvq_saveursdicietdailleurs
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Defi)
+class DefiTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
