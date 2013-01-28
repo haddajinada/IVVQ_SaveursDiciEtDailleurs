14b46b404e3c02ce79072ca80d760859452650f6
annaCejalvo <anna.cejalvo@gmail.com>
2013-01-28 15:02:43 +0100

    #10 Modif du DataSource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,5 +1,38 @@
 dataSource {
     pooled = true
+	driverClassName = "com.mysql.jdbc.Driver"
+	username = "root"
+	password = ""
+}
+hibernate {
+	cache.use_second_level_cache=true
+	cache.use_query_cache=true
+	cache.provider_class='org.hibernate.cache.EhCacheProvider'
+}
+// environment specific settings
+environments {
+	development {
+		dataSource {
+			dbCreate = "update" // one of 'create', 'create-drop','update'
+			url = "jdbc:mysql://localhost/ivvq_saveursdicietdailleurs"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost/ivvq_saveursdicietdailleurs"
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:mysql://localhost/ivvq_saveursdicietdailleurs"
+		}
+	}
+}
+/* ANCIENNE VERSION SANS MYSQL _ANNA
+dataSource {
+    pooled = true
     driverClassName = "org.h2.Driver"
     username = "sa"
     password = ""
@@ -41,3 +74,4 @@ environments {
         }
     }
 }
+*/
