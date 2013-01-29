b9697f8a4dab9c15c9c890bf124409985f07e492
NADA <haddaji.nada@gmail.com>
2013-01-29 22:20:25 +0100

    #3 creation de la classe photo

---
+++ grails-app/controllers/ivvq_saveursdicietdailleurs/PhotoController.groovy
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * Saveurs d’ici et d’ailleurs allows foodies to share recipes and cooking tips from around the globe.
+ * Copyright (C) 2012 Anna CEJALVO - Nada HADDAJI KFITA - Ahou Melaine KOFFI - Marième TOURE
+ * 
+ * Saveurs d’ici et d’ailleurs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * Saveurs d’ici et d’ailleurs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not, see ww.gnu.org/licenses/agpl-3.0.htm
+ ******************************************************************************/
+package ivvq_saveursdicietdailleurs
+
+import org.springframework.dao.DataIntegrityViolationException
+
+class PhotoController {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def list() {	
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        [photoInstanceList: Photo.list(params), photoInstanceTotal: Photo.count()]
+    }
+
+    def create() {
+        [photoInstance: new Photo(params)]
+    }
+
+    def save() {
+        def photoInstance = new Photo(params)
+        if (!photoInstance.save(flush: true)) {
+            render(view: "create", model: [photoInstance: photoInstance])
+            return
+        }
+
+		flash.message = message(code: 'default.created.message', args: [message(code: 'photo.label', default: 'Photo'), photoInstance.id])
+        redirect(action: "show", id: photoInstance.id)
+    }
+
+	def viewImage() {
+		def photo = Photo.get(params.id)
+		response.contentType = "image/jpeg"
+		response.contentLength = photo?.image?.length
+		response.outputStream.write(photo?.image)
+	  }
+	
+    def show() {
+        def photoInstance = Photo.get(params.id)
+        if (!photoInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'photo.label', default: 'Photo'), params.id])
+            redirect(action: "list")
+            return
+        }
+
+        [photoInstance: photoInstance]
+    }
+
+    def edit() {
+        def photoInstance = Photo.get(params.id)
+        if (!photoInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'photo.label', default: 'Photo'), params.id])
+            redirect(action: "list")
+            return
+        }
+
+        [photoInstance: photoInstance]
+    }
+
+    def update() {
+        def photoInstance = Photo.get(params.id)
+        if (!photoInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'photo.label', default: 'Photo'), params.id])
+            redirect(action: "list")
+            return
+        }
+
+        if (params.version) {
+            def version = params.version.toLong()
+            if (photoInstance.version > version) {
+                photoInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                          [message(code: 'photo.label', default: 'Photo')] as Object[],
+                          "Another user has updated this Photo while you were editing")
+                render(view: "edit", model: [photoInstance: photoInstance])
+                return
+            }
+        }
+
+        photoInstance.properties = params
+
+        if (!photoInstance.save(flush: true)) {
+            render(view: "edit", model: [photoInstance: photoInstance])
+            return
+        }
+
+		flash.message = message(code: 'default.updated.message', args: [message(code: 'photo.label', default: 'Photo'), photoInstance.id])
+        redirect(action: "show", id: photoInstance.id)
+    }
+
+    def delete() {
+        def photoInstance = Photo.get(params.id)
+        if (!photoInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'photo.label', default: 'Photo'), params.id])
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            photoInstance.delete(flush: true)
+			flash.message = message(code: 'default.deleted.message', args: [message(code: 'photo.label', default: 'Photo'), params.id])
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'photo.label', default: 'Photo'), params.id])
+            redirect(action: "show", id: params.id)
+        }
+    }
+}
---
+++ grails-app/views/photo/_form.gsp
@@ -0,0 +1,42 @@
+<!--
+SaveurDiciEtDailleurs est une application grails qui consiste à gérer des défis culinaires.
+
+Copyright (C) 2012  by - KFITA Nada, CEJALVO Anna, TOURE Mariame, KOFFI Aho Melaine
+
+SaveurDiciEtDailleurs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+SaveurDiciEtDailleurs  is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
+--!>
+#-------------------------------------------------------------------------------
+# Saveurs d’ici et d’ailleurs allows foodies to share recipes and cooking tips from around the globe.
+# Copyright (C) 2012 Anna CEJALVO - Nada HADDAJI KFITA - Ahou Melaine KOFFI - Marième TOURE
+# 
+# Saveurs d’ici et d’ailleurs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# 
+# Saveurs d’ici et d’ailleurs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License along with this program. If not, see ww.gnu.org/licenses/agpl-3.0.htm
+#-------------------------------------------------------------------------------
+<%@ page import="ivvq_saveursdicietdailleurs.Photo" %>
+
+
+
+<div class="fieldcontain ${hasErrors(bean: photoInstance, field: 'image', 'error')} ">
+	<label for="image">
+		<g:message code="photo.image.label" default="Image" />
+		
+	</label>
+	<input type="file" id="image" name="image" />
+</div>
+
+<div class="fieldcontain ${hasErrors(bean: photoInstance, field: 'recette', 'error')} ">
+	<label for="recette">
+		<g:message code="photo.recette.label" default="Recette" />
+		
+	</label>
+	<g:textField name="recette" value="${photoInstance?.recette}"/>
+</div>
+
