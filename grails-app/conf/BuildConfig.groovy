a882247ab67d9a603eb0bc102b4642e0aa8731f4
annaCejalvo <anna.cejalvo@gmail.com>
2013-02-11 10:27:20 +0100

    #14 updated BuildConfig - pom

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Saveurs d’ici et d’ailleurs allows foodies to share recipes and cooking tips from around the globe.
- * Copyright (C) 2012 Anna CEJALVO - Nada HADDAJI KFITA - Ahou Melaine KOFFI - Marième TOURE
+ * Saveurs dï¿½ici et dï¿½ailleurs allows foodies to share recipes and cooking tips from around the globe.
+ * Copyright (C) 2012 Anna CEJALVO - Nada HADDAJI KFITA - Ahou Melaine KOFFI - Mariï¿½me TOURE
  * 
- * Saveurs d’ici et d’ailleurs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Saveurs dï¿½ici et dï¿½ailleurs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * 
- * Saveurs d’ici et d’ailleurs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ * Saveurs dï¿½ici et dï¿½ailleurs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not, see ww.gnu.org/licenses/agpl-3.0.htm
  ******************************************************************************/
@@ -24,6 +24,7 @@ grails.project.dependency.resolution = {
     }
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
+    pom true
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
@@ -46,7 +47,7 @@ grails.project.dependency.resolution = {
 
         runtime 'mysql:mysql-connector-java:5.1.20'
 		//compile ":flash-player:1.4"
-		//build ":release:2.2.0" // c'est à propos de l'approche native concernant la gestion des dépendances
+		//build ":release:2.2.0" // c'est ï¿½ propos de l'approche native concernant la gestion des dï¿½pendances
     }
 
     plugins {
