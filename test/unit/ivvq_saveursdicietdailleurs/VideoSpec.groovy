2e79d0a935faea43a60e57bddebc4177efaa2977
annaCejalvo <anna.cejalvo@gmail.com>
2013-02-13 09:59:26 +0100

    #4 correction de tests unitaires

--- test/unit/ivvq_saveursdicietdailleurs/VideoSpec.groovy
+++ test/unit/ivvq_saveursdicietdailleurs/VideoSpec.groovy
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Saveurs d’ici et d’ailleurs allows foodies to share recipes and cooking tips from around the globe.
- * Copyright (C) 2012 Anna CEJALVO - Nada HADDAJI KFITA - Ahou Melaine KOFFI - Marième TOURE
+ * Saveurs dï¿½ici et dï¿½ailleurs allows foodies to share recipes and cooking tips from around the globe.
+ * Copyright (C) 2012 Anna CEJALVO - Nada HADDAJI KFITA - Ahou Melaine KOFFI - Mariï¿½me TOURE
  * 
- * Saveurs d’ici et d’ailleurs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Saveurs dï¿½ici et dï¿½ailleurs is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * 
- * Saveurs d’ici et d’ailleurs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ * Saveurs dï¿½ici et dï¿½ailleurs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
  * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not, see ww.gnu.org/licenses/agpl-3.0.html.
  ******************************************************************************/
@@ -36,7 +36,7 @@ class VideoSpec extends Specification {
 		mockDomain(Video)
 
 		when:
-		Video video = new Video(titreRecette : "test", keyVideoYoutube : "3NxuoIIz8Pk", recette : "ma recette")
+		Video video = new Video(titreRecette : "test", keyVideoYoutube : keyVideoYoutube, recette : "ma recette")
 		video.validate()
 
 		then:
@@ -62,8 +62,6 @@ class VideoSpec extends Specification {
 		where:
 		error	  |field		     |val
 		'blank'	  |'titreRecette'	 |''
-		'blank'   |'keyVideoYoutube' |'nada'
-		'blank'	  |'recette'	     |''
-
+		'blank'   |'keyVideoYoutube' |''
 	}
 }
